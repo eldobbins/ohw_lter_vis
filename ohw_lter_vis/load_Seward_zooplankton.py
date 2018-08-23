334f1fa55aadcfb4ad657b64af7d6062e97f872c
Darren Henrichs <propyl-alcohol@usa.net>
2018-08-23 15:07:06 -0700

    Fixed incorrect import of datetime module; added optional year limiter

--- ohw_lter_vis/load_Seward_zooplankton.py
+++ ohw_lter_vis/load_Seward_zooplankton.py
@@ -10,10 +10,25 @@ a datetime column for future alignment with environmental data.
 import pandas as pd
 from datetime import datetime
 
-def make_zooplankton_dataframe(infilename='https://workspace.aoos.org/published/file/6c544f8c-6662-4298-bdcf-52029d113c61/Seward_ZooData_Calvet_%202012-2016_final.csv',):
+def make_zooplankton_dataframe(year=None):
     
-    zooplankton_data = pd.read_csv(infilename, header=0, index_col=0)
+    """Will collect the CSV file, clean it, and fit it into a pandas DataFrame
+       for use in further visualization.
+       
+       Input:
+           year = int (optional)    Limit the return dataframe to a single year
+           
+                      
+       Returns:
+           pandas.DataFrame()       The cleaned CSV file of zooplankton abundance
+           
+           
+    """
+    
 
+    infilename='https://workspace.aoos.org/published/file/6c544f8c-6662-4298-bdcf-52029d113c61/Seward_ZooData_Calvet_%202012-2016_final.csv'
+    zooplankton_data = pd.read_csv(infilename, header=0, index_col=0)
+    
     #trim off the excess columns that may appear due the CSV formatting
     keep_columns = zooplankton_data.columns[:32]
     keep_columns = keep_columns.drop('Date-Time')
@@ -22,12 +37,15 @@ def make_zooplankton_dataframe(infilename='https://workspace.aoos.org/published/
     zooplankton_data_trimmed.rename(columns={'Latitude (degrees N)': 'latitude'})
     zooplankton_data_trimmed.rename(columns={'Longitude (degrees W)': 'longitude'})
     #create the datetime column for merging with environmental/other datasets
-    zooplankton_data_trimmed['time'] = [datetime.datetime(
+    zooplankton_data_trimmed['time'] = [datetime(
                                         x[1]['Year'], 
                                         x[1]['Month'], 
                                         x[1]['Day'], 
                                         int(x[1]['Time (hh:mm:ss AM/PM)'].split(':')[0]), 
                                         int(x[1]['Time (hh:mm:ss AM/PM)'].split(':')[1]),
                                         ) for x in zooplankton_data_trimmed.iterrows()]
-    return zooplankton_data_trimmed
+    if year:
+        return zooplankton_data_trimmed.groupby('Year').get_group(int(year))
+    else:
+        return zooplankton_data_trimmed
 
