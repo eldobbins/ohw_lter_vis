1a71a400726dea6dae9a9bc7d55bc0ef833a1637
vpreston <victorialynnpreston@gmail.com>
2018-08-24 12:48:37 -0700

    Adding documentation to iooslib

--- ohw_lter_vis/ioos_lib.py
+++ ohw_lter_vis/ioos_lib.py
@@ -26,11 +26,16 @@ from netCDF4 import Dataset
 import gridgeo
 import copy
 import xarray as xr
-import cartopy.crs as ccrs
 
 class DataScraper():
     '''
-    An object with helper functions for accessing and querying data from the IOOS site
+    An object with helper functions for accessing and querying data from the IOOS site.
+    Inputs:
+        - roi (list(floats)) - region of interest in lat-lon coordinate (min_lon, max_lon, min_lat, lax_lat)
+        - start (datetime) - start date of the temporal range to study
+        - stop (datetime) - stop date of the temporal range to study
+        - target (list(strings)) - the labels to filter for
+        - models_only (boolean) - flag for scraping for observational or model data
     '''
 
     def __init__(self, roi, start, stop, target=None, models_only=False):
@@ -41,6 +46,7 @@ class DataScraper():
         self.target = target
         self.models = models_only
 
+        # Make the filter
         self.make_bbox()
         self.make_fes_filter()
 
@@ -61,10 +67,21 @@ class DataScraper():
         query_bbox_gs.plot(ax=ax, facecolor='none', edgecolor='red', linewidth=3)
 
     def make_bbox(self):
+        '''
+        Helper function to make the filter bounding box
+        '''
         crs = 'urn:ogc:def:crs:OGC:1.3:CRS84'
         self.bbox_crs = fes.BBox([self.roi[0], self.roi[2], self.roi[1], self.roi[3]], crs=crs)
 
     def adjust_roi(self, min_lon=None, max_lon=None, min_lat=None, max_lat=None):
+        '''
+        Function which allows user to adjust different aspects of the query bounding box
+        Input:
+            - min_lon (float)
+            - max_lon (float)
+            - min_lat (float)
+            - max_lat (float)
+        '''
         if min_lon is not None:
             self.min_lon = min_lon
         if max_lon is not None:
@@ -73,20 +90,38 @@ class DataScraper():
             self.min_lat = min_lat
         if max_lat is not None:
             self.max_lat = max_lat
+
+        # reset roi
         self.roi = [self.min_lon, self.max_lon, self.min_lat, self.max_lat]
+
+        # reset filter
         self.make_bbox()
         self.make_fes_filter()
 
     def update_date(self, new_start, new_end):
+        '''
+        Function to allow the user to adjust the query temporal range
+        Inputs:
+            - new_start (datetime)
+            - new_end (datetime)
+        '''
         self.start = new_start
         self.stop = new_end
         self.make_fes_filter()
 
     def update_labels(self, new_labels):
+        '''
+        Function to allow the user to adjust the labels that will be queried
+        Input:
+            - new_labels (list(strings))
+        '''
         self.target = new_labels
         self.make_fes_filter()
 
     def make_fes_filter(self):
+        '''
+        Generates the filter for querying the IOOS database
+        '''
         begin, end = fes_date_filter(self.start, self.stop)
         kw = dict(wildCard='*',escapeChar='\\',singleChar='?',propertyname='apiso:AnyText')
         if len(self.target) > 1:
@@ -100,6 +135,9 @@ class DataScraper():
             self.filter_list = [fes.And([self.bbox_crs, begin, end, prop_filt, fes.Not([fes.PropertyIsLike(literal='*cdip',**kw)]),fes.Not([fes.PropertyIsLike(literal='*grib*', **kw)])])]
 
     def get_records(self):
+        '''
+        Pulls the catalog of data through the few filter to generate a list of matching records
+        '''
         endpoint = 'https://data.ioos.us/csw'
         self.csw = CatalogueServiceWeb(endpoint, timeout=60)
         get_csw_records(self.csw, self.filter_list, pagesize=10, maxrecords=1000)
@@ -107,18 +145,16 @@ class DataScraper():
         print('Found {} records.\n'.format(len(self.csw.records.keys())))
 
     def pretty_print_records(self):
+        '''
+        Helper function for printing the record names to terminal
+        '''
         for rec in self.csw.records.keys():
             print(str(rec)+'\n')
 
-    def select_record(self):
-        #print the unique record list
-        #allow user to select the instrument(s) of interest
-        self.assets = []
-        for k,v in list(self.csw.records.items()):
-            print(u'[{}]\n {}'.format(v.title, k))
-            self.assets.append(self.csw.records[v.title])
-
     def create_database(self):
+        '''
+        Creates a PANDAS dataframe of URLs from which to query data. Checks for geolinking.
+        '''
         df = []
         for k,v in self.csw.records.items():
             df.append(pd.DataFrame(v.references))
@@ -128,6 +164,13 @@ class DataScraper():
         self.df = df
 
     def get_observations(self, silent=True):
+        '''
+        Accesses the url list from the database and pulls the data. 
+        Input:
+            - silent (boolean) Flag for setting whether or not to print the verbose status of the process
+        Output:
+            - returns a massive dataframe from all sources
+        '''
         sos_urls = [fix_series(url, self.start, self.stop) for url in self.df.url.values if 'GetObservation' in url and 'text/csv' in url]
         self.observations = []
         for url in sos_urls:
@@ -141,33 +184,22 @@ class DataScraper():
             print('Unfortunately, no valid data targets have been found.')
             return
 
+        # Combine the successfully created dataframes into a single appended frame
         obs_df = pd.DataFrame()
         for df in self.observations:
             obs_df = obs_df.append(df)
         obs_df['time'] = pd.to_datetime(obs_df.index, infer_datetime_format=True)
         return obs_df
 
-    def plot_observations(self, value):
-        with matplotlib.style.context('seaborn-notebook'):
-            fig, ax = plt.subplots(figsize=(11, 2.75))
-            colors = mpl_palette(plt.cm.Set2, n_colors=len(self.observations))
-            for k, series in enumerate(self.observations):
-                station_name = series['station_id'].iloc[0].split(':')[-1]
-                ax.plot(series.index, series[value], label=station_name, color=colors[k])
-            leg = ax.legend(loc='upper center', ncol=10)
-
-        # hours = matplotlib.dates.DateFormatter('%H:%M')
-        # ax.xaxis.set_major_formatter(hours)
-
-        # days = matplotlib.dates.DateFormatter('\n\n%Y-%m-%d')
-        # ax.xaxis.set_minor_formatter(days)
-        # ax.xaxis.set_minor_locator(matplotlib.ticker.FixedLocator([matplotlib.dates.date2num(self.start)]))
-
     def get_models(self):
+        '''
+        Function for pulling models from the url queries.
+        '''
         self.dap_urls = []
         self.grids = {}
         self.model_urls = []
 
+        # Query each url in the database
         for i, df_row in self.df.iterrows():
             row = df_row.to_dict()
             # Handle the presenece of "opendap" in the NOAA NOS-COOPS obs domain
@@ -176,16 +208,20 @@ class DataScraper():
                 if not '.html' in url:
                     self.dap_urls.append(url)
     
+        # for the valid URLs, try to pull the data
         for url in self.dap_urls:
+            # Some files may not be in the proper format. Ignore them.
             try:
                 nc = Dataset(url)
             except:
                 continue
+            # Some files may not contain relevant information, even after filtering. Ignore them.
             try:
                 temp = nc.get_variables_by_attributes(standard_name=lambda x: x in self.target)[0]
                 standard_name = temp.standard_name
             except:
                 continue
+            # Some files may not properly convert to a geo-object. Ignore them.
             try:
                 grid = gridgeo.GridGeo(nc, standard_name=standard_name)
                 title = getattr(nc, 'title', url)
@@ -196,10 +232,21 @@ class DataScraper():
         print(self.grids)
 
     def open_models(self, param_of_interest='salt', date_of_interest=None):
+        '''
+        Function for a user to more specifically query for different aspects of the model of interest.
+        Assumes that the surface is the more interesting parameter on which to slice the targets.
+        Input:
+            - param_of_interest (string) - label on which to slice the model
+            - date_of_interest (datetime) - date on which to query the model values
+        Output:
+            - None (in the event of failure)
+            - List of models slices on the input parameters
+        '''
         models = []
         if date_of_interest is None:
             date_of_interest = self.start
 
+        # For each model url, open the dataset and check to see if it has the parameters we care about
         for url in self.model_urls:
             try:
                 mod = xr.open_dataset(url)
@@ -212,13 +259,26 @@ class DataScraper():
                 elif 'depth' in mod.keys():
                     models.append(mod[param_of_interest].isel(time=np.where(mod.time.values == np.datetime64(date_of_interest))[0], depth=1))
 
+        # Inform user that the models are not compatible for the search criteria
         if len(models) == 0:
             print('Sorry, your criteria could not be met. Try adjusting your date or target!')
             return
         else:
             return models
 
+#################
+# External Helpers
+#################
+
 def fix_series(url, start, stop):
+    '''
+    Function to make url formatting consistent
+    Input:
+        - start (datetime): start date of temporal range of interest
+        - stop (datetime): end date of temporal range of interest
+    Output:
+        - formatted string
+    '''
     url_split = re.split('[&?]', url)
     new_url = []
     for line in url_split:
@@ -228,23 +288,14 @@ def fix_series(url, start, stop):
 
     return "{}?{}".format(new_url[0], '&'.join(new_url[1:]))
 
-def mpl_palette(cmap, n_colors=6):
-    brewer_qual_pals = {"Accent": 8, "Dark2": 8, "Paired": 12,
-                        "Pastel1": 9, "Pastel2": 8,
-                        "Set1": 9, "Set2": 8, "Set3": 12}
-
-    if cmap.name in brewer_qual_pals:
-        bins = np.linspace(0, 1, brewer_qual_pals[cmap.name])[:n_colors]
-    else:
-        bins = np.linspace(0, 1, n_colors + 2)[1:-1]
-    palette = list(map(tuple, cmap(bins)[:, :3]))
-
-    pal_cycle = cycle(palette)
-    palette = [next(pal_cycle) for _ in range(n_colors)]
-    
-    return palette
-
 def fetch_labels(keyword):
+    '''
+    Helper function for accessing relevant labels in the CF standards
+    Input:
+        - keyword (string) 
+    Output:
+        - list(string)
+    '''
     if keyword == 'temperature':
         return ['sea_water_temperature','sea_surface_temperature', 'sea_water_potential_temperature','equivalent_potential_temperature','sea_water_conservative_temperature','pseudo_equivalent_potential_temperature']
     elif keyword == 'salinity':
@@ -257,6 +308,16 @@ def fetch_labels(keyword):
         return ['partial_pressure_of_methane_in_sea_water','atmosphere_mass_content_of_methane','atmosphere_mole_content_of_methane','atmosphere_moles_of_methane']
 
 def fetch_dates(start_year, start_month, start_day, duration):
+    '''
+    Helper function for creating temporal range dates
+    Input:
+        - start_year (int)
+        - start_month (int)
+        - start_day (int)
+        - duration (float) -- in days
+    Output:
+        - tuple(datetime)
+    '''
     start_date = datetime(year=start_year, month=start_month, day=start_day)
     end_date = start_date + timedelta(days=duration)
     return start_date, end_date
