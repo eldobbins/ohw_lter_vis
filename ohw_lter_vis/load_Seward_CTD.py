e9d006492b6d9342e46ca4e9ea18434980025982
eldobbins <eldobbins@alaska.edu>
2018-10-08 10:04:21 -0800

    add documentation (Google format) to load functions

--- ohw_lter_vis/load_Seward_CTD.py
+++ ohw_lter_vis/load_Seward_CTD.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+This code is designed to ingest a CTD pair of data and header files from:
+https://portal.aoos.org/old/gulf-of-alaska#metadata/e25fe1f2-1c98-44f6-856f-5d61c87c0384/project/files
+
+Data are placed into a pandas dataframe and gieven a datetime column 
+for future alignment with zooplankton data.
+
 Created on Wed Aug 22 16:23:35 2018
 
 @author: EDobbins
@@ -11,19 +17,21 @@ import csv
 import requests
 
 
-def count_header_lines(datafile):
-    '''
-    These funky CTD files have a variable number of header lines at the top of
+def count_header_lines(url):
+    """ Counts header lines in a CTD file.
+    Seward Line CTD files have a variable number of header lines at the top of
     the file.  This function count the header rows by looking for the 'END' string
     that terminates it.
     
-    :param datafile: the URL of the CTD data file (string)
-    :returns nhdr: the number of header lines that need to be skipped when reading (int)
-    '''
+    Args:
+        url : the URL of the CTD data file (string)
+    Returns:
+        the number of header lines that need to be skipped when reading (int)
+    """
     
     nhdr=0
     with requests.Session() as s:
-        download = s.get(datafile)
+        download = s.get(url)
 
         decoded_content = download.content.decode('utf-8')
 
@@ -37,60 +45,130 @@ def count_header_lines(datafile):
     return(nhdr)
 
 
-def load_data(datafile):
-    '''
-    Read data from an online CSV into a pandas dataframe.  Note: the columns 
-    are hardcoded, because they are listed as separate lines in the header, but
-    they change from year to year. They should be parsed out to make it more 
-    flexible
+def get_column_names(url):    # start the list of column headers
+    """ Gets column names from the header.
+    Seward Line CTD files have a section in the header lines that define
+    what the columns are in the file.  This function parses those by
+    looking for the strings that mark the section's beginning and end. What it 
+    finds is broken into two parts: the SBE defined variable code, and a
+    descriptive string that includes the units.
     
-    :param datafile: the URL of the CTD data file (string)
-    :returns data: the data from the file (Pandas DataFrame)
-    '''
-    # there are 30 columns whose names should be generated from the header. hardcode for now. 
-    colnames = [ 'id', 'pressure', 'temperature', 'temperature2', 'conductivity','conductivity2', 
-           'v0', 'v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7',
-           'fluorescence', 'beam_trans', 'oxygen', 
-           'altimeter', 'latitude', 'longitude', 'density', 'density2',
-           'salinity', 'salinity2', 'nbin', 'flag']
-    nhdr = count_header_lines(datafile)
-    data = pd.read_table(datafile, skiprows=nhdr, names=colnames, delim_whitespace=True)
-    return data
+    TODO: There is a kludged section that shifts some of the variable names
+     to what the notebooks in this package are expecting.  This should be 
+     refined in the future.
+    
+    Args:
+        url : the URL of the CTD data file (string)
+    Returns:
+        a tuple containing 2 lists of strings:
+            the SBE variable names that came from the .cnv files
+            the SBE variable descriptions that include units etc.
+    """
+    
+    NAMES = False  # this will trigger at the beginning of variable names section
+    varnames = []
+    vartitles = []
+    
+    # with open(datafile, 'r') as csvfile:
+    with requests.Session() as s:
+        download = s.get(url)
+
+        decoded_content = download.content.decode('utf-8')
+
+        cr = csv.reader(decoded_content.splitlines(), delimiter=':')
+        for row in cr:
+            if NAMES and row[0][1] == '%':  # names section ends with row of %
+                break
+            if NAMES:
+                index = row[0].replace('% ', '')   # the first column is the index
+                varnames.append(row[1].strip())
+                vartitles.append(row[-1].strip())
+                if int(index) != len(varnames):    # if the index doesn't match number of variables
+                    print('you gotta problem')
+            if row[0].find('Data File Column Contents')>1: NAMES = True
+    
+    # before return it, make some variable names like they were before so 
+    # notebooks don't break
+    varnames = [v.replace('Consecutive Station Number', 'id') for v in varnames]
+    varnames = [v.replace('prDM', 'pressure') for v in varnames]
+    varnames = [v.replace('t090C', 'temperature') for v in varnames]
+    varnames = [v.replace('sal00', 'salinity') for v in varnames]
+    
+    return (varnames, vartitles)
 
 
-def load_header(hdrfile):
-    '''
+def load_data(url):
+    """  Reads data from an online CSV into a pandas dataframe.  
+    Note: the columns 
+    are parsed out of the data file header.
+    
+    Args:
+        url : the URL of the CTD data file (string)
+    Returns:
+        a pandas.DataFrame that is the data from the file
+    """
+    
+    # There are 30 columns names that were originally hardcoded during OHW.
+    # The notebooks still expect these names.
+#    colnames = [ 'id', 'pressure', 'temperature', 'temperature2', 'conductivity','conductivity2', 
+#           'v0', 'v1', 'v2', 'v3', 'v4', 'v5', 'v6', 'v7',
+#           'fluorescence', 'beam_trans', 'oxygen', 
+#           'altimeter', 'latitude', 'longitude', 'density', 'density2',
+#           'salinity', 'salinity2', 'nbin', 'flag']
+    
+    # Get some data from the header lines that are used as arguments to read 
+    nhdr = count_header_lines(url)
+    (colnames, vartitles) = get_column_names(url)
+
+    return pd.read_table(url, skiprows=nhdr, names=colnames, delim_whitespace=True)
+
+
+def load_header(url):
+    """ Reads station information into a pandas dataframe.
     Read station information from an online CSV (called a header file) and put
     it into a pandas dataframe.  Note: the columns must be hardcoded, because 
     nothing is listed within the file.  Always seem to be the same.
     
-    :param hdrfile: the URL of the CTD header file (string)
-    :returns hdata: the data from the file (Pandas DataFrame)
-    '''
+    Args:
+        url : the URL of the CTD header file (string)
+    Returns:
+        a pandas.DataFrame that is the station data from the file 
+    """
+    
+    # define the column names
     hcolnames = [ 'id', 'station', 'date', 'latitude', 'longitude','waterdepth', 
            'filename', 'instrument', 'ship', 'cruise', 'junk1', 'PI', 'purpose',
            'agency', 'region', 'junk2']
-    hdata = pd.read_csv(hdrfile, delimiter=',', names=hcolnames)
+    
+    # read the data and set the index
+    hdata = pd.read_csv(url, delimiter=',', names=hcolnames)
     hdata = hdata.set_index('id')
     return hdata
 
 
 def make_CTD_dataframe():
-    '''
-    Read data and station info from online CSV files into a pandas dataframes
-    and then combine them to make a single file.
+    """ Returns CTD data from the Seward Line.
+    Read data and station info from online CSV files and combines them into 
+    to make a single pandas dataframes.
     
-    :returns df: the CTD data and station information (Pandas DataFrame)
-    '''
+    TODO: this function only returns a single year because the URLs include 
+     a hash that cannot be predicted.  Eventually, one could extend it to 
+     other years by recording all the required URLs.
+    
+    Args:
+        none
+    Returns:
+        a pandas.DataFrame that combines the CTD data and station information
+    """
 
-    # data file names gotten by navigating via aoos portal to files, then copy link
-    # still good?
-    hdrfile = 'https://workspace.aoos.org/published/file/6be0d8f6-5ddc-4ad9-90d3-8a63d5d58752/TXS12.hdr'
-    datafile = 'https://workspace.aoos.org/published/file/62874c7d-d4ac-4d59-b349-cc402d872d7f/TXS12.ascii'
+    # data file URLs were copied from links in the AOOS portal:
+    # https://portal.aoos.org/old/gulf-of-alaska.php#metadata/e25fe1f2-1c98-44f6-856f-5d61c87c0384/project
+    hdrurl = 'https://workspace.aoos.org/published/file/6be0d8f6-5ddc-4ad9-90d3-8a63d5d58752/TXS12.hdr'
+    dataurl = 'https://workspace.aoos.org/published/file/62874c7d-d4ac-4d59-b349-cc402d872d7f/TXS12.ascii'
 
     # load the two separate files
-    data= load_data(datafile)
-    hdata = load_header(hdrfile)
+    data= load_data(dataurl)
+    hdata = load_header(hdrurl)
 
     # join these two together so that data are associated with positions
     df = pd.merge(data,hdata, on='id')
@@ -99,6 +177,13 @@ def make_CTD_dataframe():
     df = df.rename(columns={'latitude_y':'latitude','longitude_y': 'longitude'})
     return df
 
+
+def main():
+    make_CTD_dataframe()
+
+
+if __name__== "__main__":
+    main()
 
 
 
--- ohw_lter_vis/load_Seward_zooplankton.py
+++ ohw_lter_vis/load_Seward_zooplankton.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-This code is designed to ingest the zooplankton file from here:
-    https://workspace.aoos.org/published/file/6c544f8c-6662-4298-bdcf-52029d113c61/Seward_ZooData_Calvet_2012-2016_final.csv
-and trim it, clean it, and place it into a pandas dataframe. I will also create
-a datetime column for future alignment with environmental data.
+This code is designed to ingest a zooplankton file from:
+https://portal.aoos.org/old/gulf-of-alaska#metadata/e25fe1f2-1c98-44f6-856f-5d61c87c0384/project/files
+
+Data are trimmed to the correct date range, cleaned, placed into a pandas 
+dataframe, and given a datetime column is also created for future alignment 
+with CTD data.
+
+Created on Wed Aug 22 16:23:35 2018
+
 """
 
 #import the necessary libraries
@@ -11,34 +16,32 @@ import pandas as pd
 from datetime import datetime
 
 def make_zooplankton_dataframe(year=None):
-    
-    """Will collect the CSV file, clean it, and fit it into a pandas DataFrame
-       for use in further visualization.
+    """ Makes a pandas dataframe from zooplankton data.
+    Will collect the CSV file from a URL, clean it, and put it in a pandas 
+    DataFrame for use in further visualization.
        
-       Input
-       -----
-           year = int (optional)    Limit the return dataframe to a single year
-                                    Must be in the 2012 - 2016 timeframe
-           
-                      
-       Returns
-       -------
-           m : pandas.DataFrame       The cleaned CSV file of zooplankton abundance
-           
-           
+    Args:
+        year : int (optional) Limit the return dataframe to a single year
+               Must be in the 2012 - 2016 timeframe
+    Returns:
+        a pandas.DataFrame that is the cleaned zooplankton abundance
     """
        
-    infilename= 'https://workspace.aoos.org/published/file/6c544f8c-6662-4298-bdcf-52029d113c61/Seward_ZooData_Calvet_2012-2016_final.csv'
-    zooplankton_data = pd.read_csv(infilename, header=0, index_col=0, encoding='latin_1')
+    # read the data
+    dataurl = 'https://workspace.aoos.org/published/file/6c544f8c-6662-4298-bdcf-52029d113c61/Seward_ZooData_Calvet_2012-2016_final.csv'
+    zooplankton_data = pd.read_csv(dataurl, header=0, index_col=0, 
+                                   encoding='latin_1')
     
-    #trim off the excess columns that may appear due the CSV formatting
+    # trim off the excess columns that may appear due the CSV formatting
     keep_columns = zooplankton_data.columns[:32]
     keep_columns = keep_columns.drop('Date-Time')
-    keep_columns
     zooplankton_data_trimmed = pd.DataFrame(zooplankton_data, columns=keep_columns)
+    
+    # rename variables to match those in CTD file
     zooplankton_data_trimmed.rename(columns={'Latitude (degrees N)': 'latitude'}, inplace=True)
     zooplankton_data_trimmed.rename(columns={'Longitude (degrees W)': 'longitude'}, inplace=True)
-    #create the datetime column for merging with environmental/other datasets
+    
+    # create the datetime column for merging with environmental/other datasets
     zooplankton_data_trimmed['time'] = [datetime(
                                         x[1]['Year'], 
                                         x[1]['Month'], 
@@ -46,13 +49,15 @@ def make_zooplankton_dataframe(year=None):
                                         int(x[1]['Time (hh:mm:ss AM/PM)'].split(':')[0]), 
                                         int(x[1]['Time (hh:mm:ss AM/PM)'].split(':')[1]),
                                         ) for x in zooplankton_data_trimmed.iterrows()]
+    
+    # trim by year
     if year:
         if 2012 <= int(year) <= 2016:
-            return zooplankton_data_trimmed.groupby('Year').get_group(int(year))
+            zooplankton_data_trimmed = zooplankton_data_trimmed.groupby('Year').get_group(int(year))
         else:
             print('That year is not included in the dataset')
-            return None
-    else:
-        return zooplankton_data_trimmed
+            zooplankton_data_trimmed = None
+    
+    return zooplankton_data_trimmed
     
 
